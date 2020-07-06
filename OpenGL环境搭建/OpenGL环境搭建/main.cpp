a6d52590e895cf253f876ac8abf95050aef0b4e1
wuchuang <1244775319@qq.com>
2020-07-06 17:32:32 +0800

    no message

--- OpenGL环境搭建/OpenGL环境搭建/main.cpp
+++ OpenGL环境搭建/OpenGL环境搭建/main.cpp
@@ -14,75 +14,109 @@ GLBatch triangleBatch;
 
 GLShaderManager shaderManager;
 
+GLfloat blockSize = 0.1f;
+GLfloat vVerts[] = {
+    -blockSize,-blockSize,0.0f,
+    blockSize,-blockSize,0.0f,
+    blockSize,blockSize,0.0f,
+    -blockSize,blockSize,0.0f,
+};
 //窗口大小改变时接受新的宽度和高度，其中0,0代表窗口中视口的左下角坐标，w，h代表像素
 void ChangeSize(int w,int h) {
-    
-    glViewport(0,0, w, h);
+    printf("%d,%d\n",w,h);
+    glViewport(0, 0, w, h);
 }
 
 //为程序作一次性的设置
 void SetupRC() {
-    //设置背影颜色
-    glClearColor(0.0f,1.0f,1.0f,0.0f);
+    glClearColor(0, 1, 1, 1);
     
-    //初始化着色管理器
+    
     shaderManager.InitializeStockShaders();
-    
-    //设置三角形，其中数组vVert包含所有3个顶点的x,y,笛卡尔坐标对。
-    GLfloat vVerts[] = {
-        -0.5f,0.0f,0.0f,
-        
-        0.5f,0.0f,0.0f,
-
-        0.0f,0.5f,0.0f,
-    };
-    //批次处理
-    triangleBatch.Begin(GL_TRIANGLES,3);
-    
+    triangleBatch.Begin(GL_TRIANGLE_FAN, 4);
     triangleBatch.CopyVertexData3f(vVerts);
-    
     triangleBatch.End();
 }
 
 //开始渲染
 void RenderScene(void) {
-    //清除一个或一组特定的缓冲区
-    glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT|GL_STENCIL_BUFFER_BIT);
-    
-    //设置一组浮点数来表示紫色
-    GLfloat vRed[] = {1.0f,0.0f,1.0f,1.0f};
-    
-    //传递到存储着色器，即GLT_SHADER_IDENTITY着色器，这个着色器只是使用指定颜色以默认笛卡尔坐标第在屏幕上渲染几何图形
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
+    GLfloat vRed[] = {1,0,0,1};
     shaderManager.UseStockShader(GLT_SHADER_IDENTITY,vRed);
-    
-    //提交着色器
     triangleBatch.Draw();
-    
-    //将在后台缓冲区进行渲染，然后在结束时交换到前台
     glutSwapBuffers();
 }
-
+void SpecialKeys(unsigned char key,int x,int y){
+    //根据相对顶点D，计算出ABCD每个顶点坐标值
+    GLfloat stepSize = 0.025f;
+    GLfloat blockX = vVerts[9];
+    GLfloat blockY = vVerts[10];
+    switch (key) {
+        case 'w'://上
+            {
+                blockY+=stepSize;
+                blockY = (blockY>1.0?1.0:blockY);
+            }
+            break;
+        case 's'://下
+            {
+                blockY-=stepSize;
+                blockY = ((blockY-blockSize*2)<-1.0?-1.0+blockSize*2:blockY);
+            }
+            break;
+        case 'a'://左a
+            {
+                blockX-=stepSize;
+                blockX = (blockX<-1.0?-1.0:blockX);
+            }
+        break;
+        case 'd'://右
+            {
+                blockX+=stepSize;
+                blockX = ((blockX+blockSize*2)>1.0?1.0-blockSize*2:blockX);
+            }
+        break;
+        default:
+            break;
+    }
+    
+    vVerts[0]=blockX;
+    vVerts[1]=blockY-blockSize*2;
+    
+    vVerts[3]=blockX+blockSize*2;
+    vVerts[4]=blockY-blockSize*2;
+    
+    vVerts[6]=blockX+blockSize*2;
+    vVerts[7]=blockY;
+    
+    vVerts[9]=blockX;
+    vVerts[10]=blockY;
+    triangleBatch.CopyVertexData3f(vVerts);
+    glutPostRedisplay();
+}
+void CloseWindow(){
+    printf("关闭窗口\n");
+    glutDestroyWindow(glutGetWindow());
+//    exit(0);
+}
 int main(int argc,char* argv[]) {
     //设置当前工作目录，针对MAC OS X
     gltSetWorkingDirectory(argv[0]);
-    
     //初始化GLUT库
     glutInit(&argc, argv);
-    
     /*初始化双缓冲窗口，其中标志GLUT_DOUBLE、GLUT_RGBA、GLUT_DEPTH、GLUT_STENCIL分别指
      双缓冲窗口、RGBA颜色模式、深度测试、模板缓冲区*/
     glutInitDisplayMode(GLUT_DOUBLE|GLUT_RGBA|GLUT_DEPTH|GLUT_STENCIL);
     
     //GLUT窗口大小，标题窗口
-    glutInitWindowSize(800,600);
+    glutInitWindowSize(500,500);
     
     glutCreateWindow("Triangle");
-    
     //注册回调函数
     glutReshapeFunc(ChangeSize);
-    
     glutDisplayFunc(RenderScene);
-    
+    glutKeyboardFunc(SpecialKeys);
+//    glutWMCloseFunc(CloseWindow);
     //驱动程序的初始化中没有出现任何问题。
     GLenum err = glewInit();
     
